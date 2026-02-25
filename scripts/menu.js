8fb621f570036d2e21b8ec7c52b792a063b700ce
GuRosendo <gustavorosendowork@gmail.com>
2026-02-24 22:56:26 -0300

    :sparkles: Feat: Footer e tela inicial responsivos

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,9 +1,12 @@
 const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
 
 window.addEventListener("scroll", () => {
   if(window.scrollY > 50){
     menu.classList.add("scrolled");
+    logo.src = "images/light-anix-full-logo-without-bg.png";
   }else{
     menu.classList.remove("scrolled");
+    logo.src = "images/light-anix-full-logo-without-bg.png";
   }
 });
